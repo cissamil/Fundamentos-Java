8a105c21c558890b9012d4803e0b3ff6bc7bb322
“cissamil“ <clarissamorita1@gmail.com>
2026-01-21 18:41:37 -0300

    Estrutura de pastas

---
+++ Interface/Areas/Formas.java
@@ -0,0 +1,12 @@
+/*    calcular a área de uma figura geométrica, 
+
+    Quadrado: possui o valor dos seus lados;
+    Retângulo: possui os valores de base e altura;
+    Circulor: possui o atributo raio.
+ */
+
+package Interface.Areas;
+
+public class Formas {
+    
+}
---
+++ Interface/Produtos/Impostos.java
@@ -0,0 +1,15 @@
+/*  
+   calcule o valor de tributos de produtos
+    Todos os produtos devem ter um método para retornar o seu valor de imposto
+    Alimentação 1%;
+    Saude e bem estar 1.5%;
+    Vestuário 2.5%;
+    Cultura 4%.
+ */
+
+
+package Interface.Produtos;
+
+public class Impostos {
+    
+}
